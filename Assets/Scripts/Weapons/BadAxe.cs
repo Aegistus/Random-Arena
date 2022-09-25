4800cc8acecf707a4e75fb470ca653bd56bcafb3
Aegistus <joe.ballanders@gmail.com>
2022-09-25 14:49:05 -0400

    Add Bad Axe Attack Damage

--- Assets/Scripts/Weapons/BadAxe.cs
+++ Assets/Scripts/Weapons/BadAxe.cs
@@ -6,7 +6,11 @@ namespace Game
 {
 	public class BadAxe : Weapon
 	{
+		[SerializeField] float damage = 20f;
+
 		Animator anim;
+		bool attacking = false;
+		float attackTime = .75f;
 
 		void Awake()
 		{
@@ -16,11 +20,32 @@ namespace Game
 	    public override void StartAttack()
 		{
 			anim.Play("Attack");
+			attacking = true;
+		}
+
+		IEnumerator AttackTimer()
+		{
+			yield return new WaitForSeconds(attackTime);
+			attacking = false;
 		}
 
 		public override void EndAttack()
 		{
-			print("End attack");
+			//print("End attack");
+		}
+
+		void OnTriggerEnter(Collider other)
+		{
+			print("Trigger entered");
+			if (attacking)
+			{
+				Health health = other.GetComponentInParent<Health>();
+				if (health != null)
+				{
+					health.Damage(damage);
+					attacking = false;
+				}
+			}
 		}
 	}
 }
