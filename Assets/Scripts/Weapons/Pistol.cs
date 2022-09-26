ac1e8c8dec14f528e516dcd15346d55e5083c78d
Aegistus <joe.ballanders@gmail.com>
2022-09-26 14:03:55 -0400

    Add Basic Pistol

---
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+	public class Pistol : Weapon
+	{
+		
+		public override void StartAttack()
+		{
+			print("PEW");
+		}
+
+		public override void EndAttack()
+		{
+
+		}
+	}
+}
+
