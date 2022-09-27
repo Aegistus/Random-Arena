8d5cd95b3899ac5354a0d97654c2c1de41933279
Aegistus <joe.ballanders@gmail.com>
2022-09-26 21:06:42 -0400

    Add Crosshair Movement Smoothing

--- Assets/Scripts/UI/CrosshairUI.cs
+++ Assets/Scripts/UI/CrosshairUI.cs
@@ -9,6 +9,7 @@ namespace Game
 	{
 		public RectTransform crosshair;
 		public TMP_Text distanceIndicator;
+		public float movementSmooth = 5f;
 
 		RaycastHit rayHit;
 		PlayerAttack playerAttack;
@@ -27,11 +28,11 @@ namespace Game
 			{
 				if (Physics.Raycast(playerAttack.currentWeapon.transform.position, playerAttack.currentWeapon.transform.forward, out rayHit, 100f))
 				{
-					crosshair.position = mainCamera.WorldToScreenPoint(rayHit.point);
+					crosshair.position = Vector3.Lerp(crosshair.position, mainCamera.WorldToScreenPoint(rayHit.point), movementSmooth * Time.deltaTime);
 				}
 				else 
 				{
-					crosshair.position = mainCamera.WorldToScreenPoint(playerAttack.currentWeapon.transform.forward * 100);
+					crosshair.position = Vector3.Lerp(crosshair.position, mainCamera.WorldToScreenPoint(playerAttack.currentWeapon.transform.forward * 100), movementSmooth * Time.deltaTime);
 				}
 			}
 			distanceIndicator.text = ((int)Vector3.Distance(playerAttack.transform.position, rayHit.point)) + "m";
