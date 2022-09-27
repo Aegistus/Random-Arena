1001fa6c7ff98cc234ec63ef3a90283a96a71aab
Aegistus <joe.ballanders@gmail.com>
2022-09-27 13:02:53 -0400

    Disable Crosshair When Using Bad Axe

--- Assets/Scripts/UI/CrosshairUI.cs
+++ Assets/Scripts/UI/CrosshairUI.cs
@@ -10,21 +10,36 @@ namespace Game
 		public RectTransform crosshair;
 		public TMP_Text distanceIndicator;
 		public float movementSmooth = 5f;
+		public bool crosshairEnabled = true;
 
 		RaycastHit rayHit;
 		PlayerAttack playerAttack;
 		Camera mainCamera;
 
-		void Start()
+		void Awake()
 		{
 			playerAttack = FindObjectOfType<PlayerAttack>();
+			playerAttack.OnWeaponChange += UpdateWeapon;
 			mainCamera = Camera.main;
 		}
 
+		void UpdateWeapon(Weapon weapon)
+		{
+			if (weapon is BadAxe)
+			{
+				crosshairEnabled = false;
+				crosshair.gameObject.SetActive(false);
+			}
+			else
+			{
+				crosshairEnabled = true;
+				crosshair.gameObject.SetActive(true);
+			}
+		}
 		
 		void Update()
 		{
-			if (playerAttack.currentWeapon != null)
+			if (playerAttack.currentWeapon != null && crosshairEnabled)
 			{
 				if (Physics.Raycast(playerAttack.currentWeapon.transform.position, playerAttack.currentWeapon.transform.forward, out rayHit, 100f))
 				{
