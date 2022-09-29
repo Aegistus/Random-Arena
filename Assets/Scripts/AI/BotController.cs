b8d74943fd7116a08c51274139ab9c42420574ee
Aegistus <joe.ballanders@gmail.com>
2022-09-28 21:25:22 -0400

    Add Bot Melee Attack

--- Assets/Scripts/AI/BotController.cs
+++ Assets/Scripts/AI/BotController.cs
@@ -10,14 +10,17 @@ namespace Game
 		[SerializeField] float attackRange = 1f;
 
 	    protected BotMovement movement;
+		protected BotAttack attack;
 		protected GameObject target;
 
 		void Start()
 		{
 			target = FindObjectOfType<PlayerHealth>().gameObject;
 			movement = GetComponent<BotMovement>();
+			attack = GetComponent<BotAttack>();
 			movement.SetDestination(target.transform.position);
 			StartCoroutine(UpdateTargetLocation());
+			StartCoroutine(CheckForAttack());
 		}
 
 		IEnumerator UpdateTargetLocation()
@@ -36,7 +39,7 @@ namespace Game
 				yield return new WaitForSeconds(updateInterval);
 				if (Vector3.Distance(transform.position, target.transform.position) < attackRange)
 				{
-					
+					attack.StartAttack();
 				}
 			}
 		}
