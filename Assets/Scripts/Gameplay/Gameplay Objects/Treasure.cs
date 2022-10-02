ec88b8119c2fa48db1feb1c41d1d6eed994b76a5
Aegistus <joe.ballanders@gmail.com>
2022-10-02 17:49:03 -0400

    Add Task Complete Sound On Treasure Collected

--- Assets/Scripts/Gameplay/Gameplay Objects/Treasure.cs
+++ Assets/Scripts/Gameplay/Gameplay Objects/Treasure.cs
@@ -51,6 +51,7 @@ namespace Game
 			{
 				Collected = true;
 				model.SetActive(false);
+				SoundManager.instance.PlaySoundAtPosition("Task Complete", transform.position);
 				TreasuresRemaining--;
 			}
 		}
